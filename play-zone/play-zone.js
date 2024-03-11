f9dc20b9f756b9d86f38b8ab02dfc8c3bf108eaa
kenzay <imdanya946@gmail.com>
2024-03-11 22:54:07 +0200

    Do Play Zone page, but not finish logo and more

--- play-zone/play-zone.js
+++ play-zone/play-zone.js
@@ -1,2 +1,2 @@
-document.querySelector(".footer").classList.add("hidden");
-document.querySelector("body").style.height = "10hv";
+// document.querySelector(".footer").classList.add("hidden");
+// document.querySelector("body").style.height = "10hv";
