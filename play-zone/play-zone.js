85258dc5a0591509c487e0657be37dfdd7c20db4
kenzay <imdanya946@gmail.com>
2024-03-26 12:24:44 +0200

    Add module window for each game

--- play-zone/play-zone.js
+++ play-zone/play-zone.js
@@ -1,2 +1,23 @@
-// document.querySelector(".footer").classList.add("hidden");
-// document.querySelector("body").style.height = "10hv";
+const moduleWindow = document.querySelector(".module");
+const overlay = document.querySelector(".module-shadow");
+const gameWindow = document.querySelectorAll(
+  ".info .name, .info .container-img"
+);
+const closeModule = moduleWindow.querySelector(".close-module");
+
+const openModuleFunction = function () {
+  moduleWindow.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModuleFunction = function () {
+  moduleWindow.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+gameWindow.forEach((el) => {
+  console.log(el);
+  el.addEventListener("click", openModuleFunction);
+});
+
+closeModule.addEventListener("click", closeModuleFunction);
